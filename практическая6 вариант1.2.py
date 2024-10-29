a90b920cc4ded04ee474f8e18f31458f1aabebf6
AleksLys <aleks.vanger@gmail.com>
2024-10-29 20:53:40 +0300

    Add files via upload

---
+++ практическая6 вариант1.2.py
@@ -0,0 +1,14 @@
+elements = int(input("Введите количество эелементов массива: "))
+a = []
+zeros = a.count(0)
+mean = elements / (elements - zeros) 
+
+for i in range(elements):
+    print("Введите", i, "элемент: ")
+    a.append(int(input()))
+print("Исходный массив: ", a)
+
+for i in range(elements):
+    if a[i] == 0:
+        a[i] = mean 
+print("Полученный массив: ", a) 
